f81653e526c0ae23d7b555f3121d5a5d190fe809
amauryxd <cdmx2472@amerike.edu.mx>
2024-01-23 20:07:44 -0600

    los cambios de playasho
    :bee:

---
+++ PlayashoRes/Player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED = 300.0 # velocidad del jugador
+
+func _physics_process(delta):
+	#metemos la deteccion de inputs
+	var direction = Input.get_axis("MovimientoGamingArriba", "MovimientoGamingAbajo")
+	if direction:
+		velocity.y = direction * SPEED #si hay un input, decimos que se mueva
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED) # cancelamos cuelquier movimiento
+
+	move_and_slide() #funcion que sirve para el movimeinto no entiendo la vea
---
+++ Scripts/MiniJuegoPlayasho.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var obstacle = preload("res://PlayashoRes/area_2d.tscn") #precargar la escena de los obstaculos
+
+var max_obstacle = 100 # cantidad maxima de obstaculos
+
+func _ready():
+	$Timer.start()
+
+func _on_timer_timeout():
+	var Obstacle_Scene=obstacle.instantiate() 
+	randomize()
+	Obstacle_Scene.global_position = Vector2(1218,randi()%600)
+	$obstacle_container.add_child(Obstacle_Scene)
