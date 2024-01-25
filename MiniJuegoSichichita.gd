1f9ee350ce50d08628db4febc46e3fab1f2330f8
Kaseem <chavarria.martinez.victor101@gmail.com>
2024-01-24 23:14:31 -0600

    Temporizador + Random max size
    El problema es que no logré que el temporizador se viera en la pantalla y que los sprites crecieran por separado, por el momento crecen juntos, pero a los dos les afecta de forma distinta el custom_max_size.
    
    Falta agregar los puntos y que se muestren en pantalla y que se muestre el temporizador en pantalla.

--- MiniJuegoSichichita.gd
+++ MiniJuegoSichichita.gd
@@ -2,20 +2,19 @@ extends Sprite2D
 # Called when the node enters the scene tree for the first time.
 var initial_scale:=Vector2(1,1) #Escala inicial del sprite.
 var growth_factor:=1.2 #Crecimiento del sprite cuando es clickeado.
-var max_size:= 5.0 #Valor máximo de crecimiento
+var custom_max_size:= 0.0 #Valor máximo de crecimiento individual.
 
 func _ready():
 	initial_scale=scale
 	set_process_input(true)
+	custom_max_size=randf_range(2.0, 5.0) #Valor máximo individual.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
 func _input(event): #detecta si el botón del mouse se ha presionado
-	if event is InputEventMouseButton and event.pressed:_on_GrowOnClick_mouse_button_pressed()
+	if event is InputEventMouseButton and event.pressed:
+		_on_GrowOnClick_mouse_button_pressed()
 
 func _on_GrowOnClick_mouse_button_pressed(): #esto se ejecuta al dar click en el mouse sobre el sprite.
 	scale*=growth_factor
 	#Condicional que verifica si el sprite está en el tamaño máximo, para ocultarlo.
-	if scale.x>=max_size or scale.y >=max_size:
+	if scale.x>=custom_max_size or scale.y >=custom_max_size:
 		hide()
