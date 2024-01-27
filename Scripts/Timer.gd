5f17643548f8e975c8eac5c1598e88c6d735aaa2
Kaseem <chavarria.martinez.victor101@gmail.com>
2024-01-26 22:17:30 -0600

    MiniJuegoSichichita V1.2
    - Se arregló el problema del canvas al expandir o minimizar la pestaña.
    - El temporizador ahora se puede ver en el canvas.
    - El puntaje se puede ver en el Canvas.
    - Al terminar el tiempo aparece un Canvas adicional que pone un mensaje y un botón para continuar con ls historia.
    
    Falta:
    - Arreglar los pixeles, para que se vean nítidos.
    - Sistema funcional de puntos.

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -1,21 +1,38 @@
 extends Node2D
 
-var time_left = 60
+var time_left = 5
 var timer_running = true
+var time_label # Referencia al nodo Label
+var panel # Referencia al nodo Panel
+var control # Referencia al nodo Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Iniciar el temporizador
-	$Timer.start()
+	# Obtener la referencia al nodo Label, Panel y Control
+	time_label = $Time #Este es el nodo donde se muestra el tiempo restante.
+	panel = $Control/Panel
+	control = $Control
+	
+	# Ocultar el panel al principio
+	panel.visible = false
+	# Desactivar la interacción con el control al principio
+	control.set_process_input(false)
 
 # Función para manejar la lógica del temporizador
 func _process(delta):
 	if timer_running:
 		time_left -= delta
-	# Verificar si el tiempo ha llegado a cero
-	if time_left <= 0:
-		print("¡Tiempo agotado!")
-		# Puedes agregar acciones adicionales aquí cuando el tiempo llega a cero
-		timer_running = false
-	else:
-		print("Tiempo restante: ", time_left)
+		# Verificar si el tiempo ha llegado a cero
+		if time_left <= 0:
+			print("¡Se acabó!")
+			# Más acciones cc el tiempo llege a 0.
+			time_label.text = "¡Se acabó!"
+			timer_running = false
+			# Mostrar el panel
+			panel.visible = true
+			# Activar la interacción con el control
+			control.set_process_input(true)
+		else:
+			# Actualizar el texto del Label (Time) con el tiempo restante
+			time_label.text = "Tiempo: " + str(int(time_left))
+			print("Tiempo: ", time_left)
