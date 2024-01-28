a8bae17d288e44246e55f54ced62b4d98add4408
wilbert Arcila <warcilac9@gmail.com>
2024-01-27 20:47:18 -0600

    matenme

--- Scripts/Interactuable.gd
+++ Scripts/Interactuable.gd
@@ -20,7 +20,7 @@ func _on_body_entered(body):
 			#return
 
 func _unhandled_input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_accept") and isTocando:
+	if Input.is_action_just_pressed("ui_accept"):
 			var actionables = actionable_finder.get_overlapping_areas()
 			if actionables.size() > 0:
 				actionables[0].action()
